d7d2da1155003326b487a95997cb382b658b5f7b
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2019-06-23 23:42:21 -0300

    Finalização do card de imóvel

--- app/src/main/java/com/rimpressao/exerciciolocacao/adapters/ImovelViewHolder.java
+++ app/src/main/java/com/rimpressao/exerciciolocacao/adapters/ImovelViewHolder.java
@@ -20,9 +20,9 @@ public class ImovelViewHolder extends RecyclerView.ViewHolder {
         nome = itemView.findViewById(R.id.imovel_titulo);
         descricao = itemView.findViewById(R.id.imovel_descricao);
         localizacao = itemView.findViewById(R.id.imovel_localizacao);
-        quantidadeQuarto = itemView.findViewById(R.id.imovel_quantidadeQuarto);
-        vagasGaragem = itemView.findViewById(R.id.imovel_vagasGaragem);
-        telefoneContato = itemView.findViewById(R.id.imovel_telefoneContato);
+        quantidadeQuarto = itemView.findViewById(R.id.imovel_quartos);
+        vagasGaragem = itemView.findViewById(R.id.imovel_vagas_garagem);
+        telefoneContato = itemView.findViewById(R.id.imovel_telefone);
         valor = itemView.findViewById(R.id.imovel_valor);
         tipo = itemView.findViewById(R.id.imovel_tipo);
     }
