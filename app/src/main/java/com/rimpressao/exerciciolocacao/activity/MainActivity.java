84bc595452d5b25371599ad5217cb0a73de6fc7a
Bruno Alves <bruno20152013@gmail.com>
2019-06-30 13:51:26 -0300

    Barra de pesquisa para filtro

--- app/src/main/java/com/rimpressao/exerciciolocacao/activity/MainActivity.java
+++ app/src/main/java/com/rimpressao/exerciciolocacao/activity/MainActivity.java
@@ -6,6 +6,13 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.rimpressao.exerciciolocacao.R;
 import com.rimpressao.exerciciolocacao.adapters.AdapterImovel;
 import com.rimpressao.exerciciolocacao.repositorio.modelo.Imovel;
@@ -16,19 +23,26 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private ImovelServico servicoImovel;
     private RecyclerView rvImoveis;
+    private EditText editText;
 
+    private ArrayList<Imovel> listaImoveis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText) findViewById(R.id.pesquisa);
+        editText.setOnEditorActionListener(editorActionListener);
+
+        getSupportActionBar().hide();
+
         carregarRecyclerViewImovel();
     }
 
     private void carregarRecyclerViewImovel() {
         servicoImovel = new ImovelServico(getApplicationContext());
-        ArrayList<Imovel> listaImoveis = servicoImovel.listarImoveis();
+        listaImoveis = servicoImovel.listarImoveis();
         AdapterImovel adapterImovel = new AdapterImovel(listaImoveis,getApplicationContext());
         rvImoveis = (RecyclerView) findViewById(R.id.lista_imoveis);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -36,5 +50,34 @@ public class MainActivity extends AppCompatActivity {
         rvImoveis.setItemAnimator(new DefaultItemAnimator());
         rvImoveis.setAdapter(adapterImovel);
         adapterImovel.notifyDataSetChanged();
+    }
+
+    private TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            switch (actionId) {
+                case EditorInfo.IME_ACTION_SEARCH:
+                    realizaPesquisa();
+                    break;
+            }
+            return false;
+        }
+    };
+
+    private void realizaPesquisa() {
+        for (Imovel item: listaImoveis) {
+            final String texto = editText.getText().toString();
+
+            Log.d("ITEM", texto);
+
+            if (editText.getText() != null) {
+                if (item != null && !item.getNome().contains(texto)) {
+                    listaImoveis.remove(item);
+                }
+            }
+            else {
+                listaImoveis = servicoImovel.listarImoveis();
+            }
+        }
     }
 }
